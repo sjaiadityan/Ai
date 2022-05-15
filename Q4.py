78c14975f6ca6ceab87958f3ab17652ec0e33e2b
sjaiadityan <99638743+sjaiadityan@users.noreply.github.com>
2022-05-15 09:16:44 +0530

    Update Q4

--- Q4.py
+++ Q4.py
@@ -1,17 +1,9 @@
 import numpy as np     
 li=[]
-while 1:
-    ch=int(input("1.enter data 2.exit: "))
-    if ch==1:
-        n=int(input('enter the number of rows :'))
-        for i in range(n):
-            name=input("enter the name: ")
-            price=input("enter the price: ")
-            rating=int(input("enter the rating(out of 5): "))
-            l=(name,price,rating)
-            li.append(l)
-        data_type = [('name', 'S15'), ('price', float), ('rating', int)]
-        food = np.array(li, dtype=data_type) 
-        print(np.sort(food, order='rating')) 
-    if ch==2:
-        break
+import csv
+with open("Q4dataset.csv",'r') as r:
+    read=csv.reader(r)
+    li=list(read)
+    li.sort(key=lambda x: x[2])
+food = np.array(li) 
+print(food) 
