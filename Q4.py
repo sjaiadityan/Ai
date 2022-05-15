535f7742ca7aec55bea1752aa62a740f093911b2
sjaiadityan <99638743+sjaiadityan@users.noreply.github.com>
2022-05-15 09:50:54 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,9 +0,0 @@
-import numpy as np     
-li=[]
-import csv
-with open("Q4dataset.csv",'r') as r:
-    read=csv.reader(r)
-    li=list(read)
-    li.sort(key=lambda x: x[2])
-food = np.array(li) 
-print(food) 
